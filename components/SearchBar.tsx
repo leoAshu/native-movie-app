8123569e64c630122ce49bb357435c15ba833fce
leoAshu <ashutosh.ojha2009@gmail.com>
2025-07-16 11:06:04 -0400

    implements home page searchbar based navigation to search page

---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { icons } from '@/constants/icons';
+import { Image, NativeSyntheticEvent, NativeTouchEvent, TextInput, View } from 'react-native';
+
+interface SearchBarProps {
+  placeholder: string;
+  onPress?: (e: NativeSyntheticEvent<NativeTouchEvent>) => void;
+}
+
+const SearchBar = (props: SearchBarProps) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#AB8BFF' />
+      <TextInput
+        value=''
+        placeholder={props.placeholder}
+        placeholderTextColor='#A8B5DB'
+        onPress={props.onPress}
+        onChangeText={() => {}}
+        className='flex-1 ml-2 text-white'
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
